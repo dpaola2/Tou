f757c4253aa48330c7b9111f7472d0a43e53a362
Dave Paola <dpaola2@gmail.com>
2012-03-30 16:30:03 -0700

    wtf

--- static/js/application.js
+++ static/js/application.js
@@ -1,4 +1,5 @@
 var setup = function() {
+    $('.editor textarea').focus();
     $('.editor textarea').on('input', function() {
         content = markdown.toHTML($('.editor textarea').val());
         $('.preview').html(content);
