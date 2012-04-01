4045ee3e09a43b67f9c18b75182c1ffa19f4e0e1
Justin Tulloss <justin.tulloss@gmail.com>
2012-03-31 19:00:57 -0700

    Scope JS
    Scope JS so we aren't leaking into the global scope

--- static/js/application.js
+++ static/js/application.js
@@ -1,42 +1,44 @@
-var focus_editor = function() {
-    $('.editor textarea').focus();    
-}
+(function($) {
+    var focus_editor = function() {
+        $('.editor textarea').focus();    
+    }
 
-var convert = function() {
-    content = markdown.toHTML($('.editor textarea').val());
-    $('.preview').html(content);
-}
+    var convert = function() {
+        content = markdown.toHTML($('.editor textarea').val());
+        $('.preview').html(content);
+    }
 
-var start_converting = function() {
-    $('.editor textarea').on('input', convert);
-}
+    var start_converting = function() {
+        $('.editor textarea').on('input', convert);
+    }
 
-var load_content = function(content) {
-    $('.editor textarea').val(content);
-    convert();
-}
+    var load_content = function(content) {
+        $('.editor textarea').val(content);
+        convert();
+    }
 
-var load_file = function(url) {
-    $.ajax({
-        url: "/load_file",
-        data: {url: url},
-        success: function(data, textStatus, jqxhr) { load_content(data); }, 
-        error: function(jqxhr, textStatus, errorThrown) { load_content(textStatus); }
-    });
-}
+    var load_file = function(url) {
+        $.ajax({
+            url: "/load_file",
+            data: {url: url},
+            success: function(data, textStatus, jqxhr) { load_content(data); }, 
+            error: function(jqxhr, textStatus, errorThrown) { load_content(textStatus); }
+        });
+    }
 
-var load_readme = function() {
-    $.ajax({
-        url: "/readme",
-        success: function(data, textStatus, jqxhr) { load_content(data); },
-        error: function(jqxhr, textStatus, errorThrown) { load_content(textStatus); }
-    });
-}
+    var load_readme = function() {
+        $.ajax({
+            url: "/readme",
+            success: function(data, textStatus, jqxhr) { load_content(data); },
+            error: function(jqxhr, textStatus, errorThrown) { load_content(textStatus); }
+        });
+    }
 
-var setup = function() {
-    focus_editor();
-    start_converting();
-    load_readme();
-}
+    var setup = function() {
+        focus_editor();
+        start_converting();
+        load_readme();
+    }
 
-$(setup)
+    $(setup)
+})(jQuery);
