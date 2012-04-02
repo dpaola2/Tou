5e97acff24cb7cc2f21abb77935aba0752e59889
Dave Paola <dpaola2@gmail.com>
2012-04-02 00:02:44 -0700

    refactored oauth code

--- app.py
+++ app.py
@@ -23,13 +23,23 @@ DROPBOX_KEY         = 'whxozioi915s2wr'
 DROPBOX_SECRET      = '9605perofirq9tg'
 DROPBOX_ACCESS_TYPE = 'dropbox'
 DROPBOX_CALLBACK    = 'http://%s/dropbox_callback' % host
-DROPBOX_SESSION_KEY = 'dropbox_token'
+DROPBOX_REQUEST_KEY = 'dropbox_request_token'
+DROPBOX_ACCESS_KEY  = 'dropbox_access_token'
+
 
 DROPBOX_SESSION     = dropbox_session.DropboxSession(
     DROPBOX_KEY,
     DROPBOX_SECRET,
     DROPBOX_ACCESS_TYPE
 )
+
+def get_session():
+    return session.DropboxSession(DROPBOX_KEY, DROPBOX_SECRET, DROPBOX_ACCESS_TYPE)
+
+def get_client(access_token):
+    sess = get_session()
+    sess.set_token(access_token.key, access_token.secret)
+    return dropbox_client.DropboxClient(sess)
 
 @app.route('/')
 def index():
@@ -46,28 +56,42 @@ def load_file():
     except Exception, e:
         return e.message
 
+@app.route('/load_dropbox_file')
+def load_dropbox_file(filepath):
+    pass
+
 @app.route('/readme')
 def readme():
     return open("README.md").read()
 
 @app.route('/link_dropbox')
 def link_dropbox():
+    if session.get(DROPBOX_ACCESS_KEY, False):
+        return redirect('/')
+    
     request_token = DROPBOX_SESSION.obtain_request_token()
+    session[DROPBOX_REQUEST_KEY] = request_token
     url = DROPBOX_SESSION.build_authorize_url(request_token, oauth_callback = DROPBOX_CALLBACK)
     return redirect(url)
 
 @app.route('/dropbox_callback')
 def dropbox_callback():
-    oauth_token = request.args.get('oauth_token', None)
-    if oauth_token is None:
+    request_token_key = request.args.get('oauth_token', None)
+    if request_token_key is None:
         return "dropbox didn't give me a token!"
 
-    session[DROPBOX_SESSION_KEY] = oauth_token
+    request_token = session.get(DROPBOX_REQUEST_KEY, None)
+    if request_token is None:
+        return "couldn't find that token key"
+    
+    access_token = DROPBOX_SESSION.obtain_access_token(request_token)
+    session[DROPBOX_ACCESS_KEY] = access_token
     return redirect('/')
 
 @app.route('/unlink_dropbox')
 def unlink_dropbox():
-    session.pop(DROPBOX_SESSION_KEY)
+    session.pop(DROPBOX_REQUEST_KEY)
+    session.pop(DROPBOX_ACCESS_KEY)
     return redirect('/')
 
 @app.route('/debug_session')
