5f9ce21068d5f58fbbe6ae2f300e03291911da40
Justin Tulloss <justin.tulloss@gmail.com>
2012-04-23 23:08:10 -0700

    make dropbox linking part of the services listing responsiblity

--- static/js/fs/services.js
+++ static/js/fs/services.js
@@ -1,10 +1,33 @@
 define(['./dropbox', './local'], function(dropbox, local) {
+    function DropboxLinker() {}
+    _.extend(DropboxLinker.prototype, {
+        read: function(callback) {
+            // TODO: do this by ajax, not redirects
+            window.location.pathname = '/dropbox/link';
+        }
+    });
+
+    function get_dropbox_service_entry() {
+        var dbSession = sessions.dropbox;
+        // TODO: resolve multiple users, 403 errors
+        /*
+        if (!dbSession) {
+            dbSession = localStorage.getItem('dropboxSession');
+        } else {
+            localStorage.setItem('dropboxSession', dbSession);
+        }*/
+        if (dbSession) {
+            return { name: 'Dropbox', type: 'dir', reader: dropbox.Directory };
+        } else {
+            return { name: 'Link Dropbox', type: 'file', reader: DropboxLinker };
+        }
+    }
+
     function ServiceDirectory() {}
     _.extend(ServiceDirectory.prototype, {
         ls: function(callback) {
             var services = [];
-            //TODO: determine if dropbox is supported
-            services.push({ name: 'Dropbox', type: 'dir', reader: dropbox.Directory });
+            services.push(get_dropbox_service_entry());
             if (local.File.supported()) {
                 local.File.initialize(function(err) {
                     if (!err) {
